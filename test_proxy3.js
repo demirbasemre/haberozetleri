89120c1a464d8f29cce38b80fd55ec821baf91a7
Emre Demirbaş <emre@Emre-MacBook-Air-2.local>
2026-05-23 11:54:53 +0300

    chore: remove non-site files from repo, add .gitignore

--- test_proxy3.js
+++
@@ -1,5 +0,0 @@
-const url = "https://api.stlouisfed.org/fred/series/observations?series_id=WJFUELUSGULF&api_key=ba29f7b58bfc036bddf0434ceb0c0534&file_type=json&limit=2&sort_order=desc";
-fetch('https://thingproxy.freeboard.io/fetch/' + url)
-  .then(r => r.json())
-  .then(d => console.log("thingproxy:", d.observations.length))
-  .catch(e => console.error("thingproxy:", e));
--- test_proxy4.js
+++
@@ -1,5 +0,0 @@
-const url = "https://api.stlouisfed.org/fred/series/observations?series_id=WJFUELUSGULF&api_key=ba29f7b58bfc036bddf0434ceb0c0534&file_type=json&limit=2&sort_order=desc&cb=1";
-fetch('https://api.allorigins.win/raw?url=' + encodeURIComponent(url))
-  .then(r => r.json())
-  .then(d => console.log("allorigins/raw:", d.observations.length))
-  .catch(e => console.error("allorigins/raw:", e));
